ba0b8bbd900a05664626fc2bdad56f1332e0fccc
MD FERDOUS AHMED <148644652+MdFerdousAhmed@users.noreply.github.com>
2026-05-19 12:28:50 +0600

    added featured

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
-require('dotenv').config();
+dotenv.config();
 const app = express();
 app.use(cors());
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 5000;
 
 
 
-const uri = process.env.MONGODB_URI;
+
+
+const uri = process.env.MONGODB_URI
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -26,6 +28,30 @@ async function run() {
     await client.connect();
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
+    
+    const db = client.db("studynookdb");
+    const roomsCollection = db.collection("rooms");
+
+    app.get("/rooms", async(req, res) =>{
+      const cursor = roomsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    app.get("/featured", async(req, res) =>{
+      const cursor = roomsCollection.find().limit(4);
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    app.get("/rooms/:roomId", async(req, res) =>{
+      const {roomId} = req.params;
+      const query = {_id: new ObjectId(roomId)}
+      const result = await roomsCollection.findOne(query);
+      res.send(result)
+      // console.log(roomId)
+    })
+
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
@@ -36,9 +62,9 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
